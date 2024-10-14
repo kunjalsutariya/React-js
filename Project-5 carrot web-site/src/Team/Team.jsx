bc6f0ddbea0d767bfb03f5dbb2fd357ccf842d38
kunja <dsutariya0007@gmail.com>
2024-10-14 10:34:56 +0530

    React-js

--- Project-5 carrot web-site/src/Team/Team.jsx
+++ Project-5 carrot web-site/src/Team/Team.jsx
@@ -6,12 +6,16 @@ const Team = () => {
         <div>
             <div className="container">
                 <div className="row">
-                    <div className="col-12 text-center mb-4">
+                    <div className="col-12 text-center mb-5">
+                    
                         <h2 className="fw-bold">Popular Products</h2>
                         <p className="text-black-50">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor <br></br>incididunt ut labore lacus vel facilisis.</p>
                     </div>
                     <div className="col-4">
                         <div className="team-card">
+                            <div className="teamimg">
+                                <img src="./img/team1.jpg" alt="" />
+                            </div>
                             <span>Co Founder</span>
                             <h4>Stephen Smith</h4>
                             <p>“eiusmpsu dolor sit amet, conse cte tur <br /> ng elit, sed do eiusmod tem lacus vel <br></br>
@@ -28,6 +32,9 @@ const Team = () => {
                     </div>
                     <div className="col-4">
                         <div className="team-card">
+                        <div className="teamimg">
+                                <img src="./img/team2.jpg" alt="" />
+                            </div>
                             <span>Manager</span>
                             <h4>Lorem Robinson</h4>
                             <p>“eiusmpsu dolor sit amet, conse cte tur <br /> ng elit, sed do eiusmod tem lacus vel <br></br>
@@ -44,6 +51,9 @@ const Team = () => {
                     </div>
                     <div className="col-4">
                         <div className="team-card">
+                        <div className="teamimg">
+                                <img src="./img/team3.jpg" alt="" />
+                            </div>
                             <span>Team Leader</span>
                             <h4>Saddika Alard</h4>
                             <p>“eiusmpsu dolor sit amet, conse cte tur <br /> ng elit, sed do eiusmod tem lacus vel <br></br>
